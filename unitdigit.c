bec6cab741eaf41ba8826b03026ac4fb419d4838
Aman Kamal <109606739+KAMALAMAN687@users.noreply.github.com>
2022-07-25 02:30:05 +0530

    Add files via upload

---
+++ unitdigit.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("enter a number :");
+    scanf("%d",&x);
+    printf("unit digit of a entered number is : %d",x%10); //it give unit digit of a given number
+    return 0;
+}
